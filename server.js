61fd874cb19f55347de4b914aca3e72c2ca3ce38
Jonathan Rupp <jrupp@rightpoint.com>
2022-09-21 12:09:14 -0500

    use simple requires

--- server.js
+++ server.js
@@ -1,7 +1,5 @@
-import { renderToStringWithData } from '@apollo/client/react/ssr';
-import { renderToString } from 'react-dom/server';
-const simple = require('react-dom/server').renderToString;
+const renderToStringWithData = require('@apollo/client/react/ssr').renderToStringWithData;
+const renderToString = require('react-dom/server').renderToString;
 
 console.log(renderToStringWithData(React.createElement('li')));
 console.log(renderToString(React.createElement('li')));
-console.log(simple(React.createElement('li')));
