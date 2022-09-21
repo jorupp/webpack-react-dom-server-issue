0ef5ae31bd4138e9ca5ba9fa17f8f2dc6438a3cf
Jonathan Rupp <jrupp@rightpoint.com>
2022-09-21 13:00:31 -0500

    fix the build output locaiton too

--- server/webpack.config.js
+++ server/webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
   entry: path.resolve(__dirname, './server.js'),
   target: 'node',
   output: {
-    path: path.resolve(__dirname, 'build'),
+    path: path.resolve(__dirname, '../build'),
     filename: 'server.bundle.js',
     libraryTarget: 'this',
   },
