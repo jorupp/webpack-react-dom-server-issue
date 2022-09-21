48ab557babee3436df3bb63bd1fbe3a300941487
Jonathan Rupp <jrupp@rightpoint.com>
2022-09-21 12:08:14 -0500

    add direct require call for comparison

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 import { renderToStringWithData } from '@apollo/client/react/ssr';
 import { renderToString } from 'react-dom/server';
+const simple = require('react-dom/server').renderToString;
 
 console.log(renderToStringWithData(React.createElement('li')));
 console.log(renderToString(React.createElement('li')));
+console.log(simple(React.createElement('li')));
